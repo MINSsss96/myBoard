0c38d8c244446c12e1d978959448c4aa970835a3
MINSsss96 <minsue0422@naver.com>
2025-08-29 13:17:39 +0900

    삭제 처리 완료

--- src/main/java/com/my/board/controller/ArticleController.java
+++ src/main/java/com/my/board/controller/ArticleController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -63,4 +64,12 @@ public class ArticleController {
         model.addAttribute("dto", dto);
         return "/articles/show";
     }
+
+    @GetMapping("{id}/delete")
+    public String deleteArticle(@PathVariable("id")Long id, RedirectAttributes redirectAttributes){
+        articleService.deleteArticle(id);
+        redirectAttributes.addFlashAttribute("msg", "정상적으로 삭제되었습니다.");
+        return "redirect:/articles";
+    }
+
 }
