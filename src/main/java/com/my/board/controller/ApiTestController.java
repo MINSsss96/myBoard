a24763d5c5ff716fc3ce8de5230a42f630a71515
MINSsss96 <minsue0422@naver.com>
2025-08-29 18:05:23 +0900

    API 초기 과정

---
+++ src/main/java/com/my/board/controller/ApiTestController.java
@@ -0,0 +1,18 @@
+package com.my.board.controller;
+
+import com.my.board.dto.LoginDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApiTestController {
+    @PostMapping("/apiTest")
+    @ResponseBody
+    public LoginDto getTest(){
+        return new LoginDto("minss", "1111");
+    }
+
+}
---
+++ src/main/java/com/my/board/dto/LoginDto.java
@@ -0,0 +1,15 @@
+package com.my.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class LoginDto {
+    private String userId;
+    private String password;
+
+}
